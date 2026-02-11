122f95b359e324d1db111f404e3288a5b9f00c78
MalyshevMS <matvey.malyshev29@gmail.com>
2026-02-11 19:10:31 +0300

    minimal tcp server

---
+++ src/config.txx
@@ -0,0 +1,4 @@
+#pragma once
+
+#define QSM_PORT                58921
+#define QSM_PACKET_SIZE         4096 // bytes
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <SFML/Network.hpp>
+#include "config.txx"
 #include <print>
 
 int main(int argc, char const *argv[]) {
-    std::println("Hello, world!");
+    sf::TcpListener listener;
+    auto _ = listener.listen(QSM_PORT, sf::IpAddress::LocalHost);
+
+    sf::TcpSocket sock;
+    if (listener.accept(sock) != sf::Socket::Status::Done) return 2;
+
+    while (true) {
+        char buff[QSM_PACKET_SIZE];
+        size_t recieved;
+
+        if (sock.receive(buff, sizeof(buff), recieved) != sf::Socket::Status::Done) break;
+
+        std::string packet {buff, recieved};
+
+        std::println("Packet recieved: {}", packet);
+    }
     return 0;
 }
